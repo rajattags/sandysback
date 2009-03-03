ec3399b5499456a5125a3319abd0f9bb6df0140e
jameshugman <jameshugman@60b3d766-bcb7-11dd-bed2-db724ec49875>
2009-03-03 22:37:14 +0000

    Moved trunk to aleaxit branch.

--- trunk/models.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import db
-import search
-
-class Fact(search.SearchableModel):
-  stuff = db.TextProperty()
-  user = db.UserProperty()
-  publishDate = db.DateTimeProperty(auto_now_add=True)
-
