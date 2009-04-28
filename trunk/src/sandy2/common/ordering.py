a81c3f5844c53c7d3daa361fed2ea221463d78f8
jameshugman <jameshugman@60b3d766-bcb7-11dd-bed2-db724ec49875>
2009-04-28 22:59:57 +0000

    Added to_dot method to display dependency graph.

--- trunk/src/sandy2/common/ordering.py
+++ trunk/src/sandy2/common/ordering.py
@@ -1,82 +1,7 @@
-
-
 __author__="james"
 __date__ ="$05-Jan-2009 22:14:29$"
 
-
-def topological_sort(list_of_nodes):
-
-    outbound_nodes = dict()
-    inbound_nodes = dict()
-    id_to_nodes = dict()
-
-    # set up an adjacency map, based on nodes which can be processed after this one.
-    # we have to do this here, rather than rely on the data given to us by the nodes,
-    # because we have to unite the incoming and outbounds of each node.
-    for node in list_of_nodes:
-        id = getattr(node, 'id', 'unknown')
-        if id is 'unknown':
-            id = node.__class__.__name__
-            setattr(node, 'id', id)
-
-        id_to_nodes[id] = node
-
-        followers = outbound_nodes.setdefault(id, set())
-        for my_follower in getattr(node, 'is_followed_by', []):
-            followers.add(my_follower)
-            inbound_nodes.setdefault(my_follower, set()).add(id)
-            outbound_nodes.setdefault(my_follower, set())
-
-        incomers = inbound_nodes.setdefault(id, set())
-        for my_preceeder in getattr(node, 'is_preceeded_by', []):
-            incomers.add(my_preceeder)
-            outbound_nodes.setdefault(my_preceeder, set()).add(id)
-            inbound_nodes.setdefault(my_preceeder, set())
-
-    schedule = []
-    ready_for_scheduling = []
-    while len(schedule) != len(list_of_nodes):
-
-        # find the next node to schedule
-        next_node_id = None
-        if len(ready_for_scheduling) > 0:
-            next_node_id = ready_for_scheduling.pop()
-        else:
-            # this loop is only run once, on first run
-            for id, incomers in inbound_nodes.items():
-                if len(incomers) == 0:
-                    del inbound_nodes[id]
-                    if next_node_id is None:
-                        next_node_id = id
-                    else:
-                        ready_for_scheduling.append(id)
-                       
-        if (next_node_id is None):
-            # we probably have a cycle, panic
-            raise CyclicDependencyError()
-
-        # we probably have node, so we can do something with it
-        next_node = id_to_nodes.get(next_node_id, None)
-        if (next_node is not None):
-            # like schedule it, if it's not a placeholder
-            schedule.append(next_node)
-
-        # now, we want to delete this node from incoming lists
-        outbound = outbound_nodes[next_node_id]
-        del outbound_nodes[next_node_id]
-
-        id = next_node_id
-        for next in outbound:
-            incoming = inbound_nodes.get(next,set())
-            incoming.discard(id)
-            if len(incoming) == 0:
-                del inbound_nodes[next]
-                ready_for_scheduling.append(next)
-
-    return schedule
-
-
-
+debug = True
 
 class INode(object):
     """A common type to allow ordering of parsers and actions.
@@ -134,3 +59,120 @@ class DependencyList:
         
     def __str__(self):
         return str(self._nodes)
+
+def topological_sort(list_of_nodes):
+    
+    if debug:
+    	print to_dot(list_of_nodes)
+    
+    id_to_nodes, inbound_nodes, outbound_nodes = _create_adjacency_matrix(list_of_nodes)
+    schedule = _create_new_node_list(id_to_nodes, inbound_nodes, outbound_nodes)
+    return schedule
+
+def _create_new_node_list(id_to_nodes, inbound_nodes, outbound_nodes):
+    schedule = []
+    ready_for_scheduling = []
+    num_nodes = len(id_to_nodes)
+    while len(schedule) != num_nodes:
+    # find the next node to schedule
+        next_node_id = None
+        if len(ready_for_scheduling) > 0:
+            next_node_id = ready_for_scheduling.pop()
+        # this loop is only run once, on first run
+        else:
+            for id, incomers in inbound_nodes.items():
+                if len(incomers) == 0:
+                    del inbound_nodes[id]
+                    if next_node_id is None:
+                        next_node_id = id
+                    else:
+                        ready_for_scheduling.append(id)
+        if (next_node_id is None):
+            # we probably have a cycle, panic
+            raise CyclicDependencyError()
+        # we probably have node, so we can do something with it
+        next_node = id_to_nodes.get(next_node_id, None)
+        if (next_node is not None):
+            # like schedule it, if it's not a placeholder
+            schedule.append(next_node)
+        # now, we want to delete this node from incoming lists
+        outbound = outbound_nodes[next_node_id]
+        del outbound_nodes[next_node_id]
+        id = next_node_id
+        for next in outbound:
+            incoming = inbound_nodes.get(next, set())
+            incoming.discard(id)
+            if len(incoming) == 0:
+                del inbound_nodes[next]
+                ready_for_scheduling.append(next)
+    return schedule
+
+
+    
+def _create_adjacency_matrix(list_of_nodes):
+    outbound_nodes = dict()
+    inbound_nodes = dict()
+    id_to_nodes = dict()
+    # set up an adjacency map, based on nodes which can be processed after this one.
+    # we have to do this here, rather than rely on the data given to us by the nodes,
+    # because we have to unite the incoming and outbounds of each node.
+    for node in list_of_nodes:
+        id = getattr(node, 'id', 'unknown')
+        if id is 'unknown':
+            id = node.__class__.__name__
+            setattr(node, 'id', id)
+        
+        id_to_nodes[id] = node
+        followers = outbound_nodes.setdefault(id, set())
+        for my_follower in getattr(node, 'is_followed_by', []):
+            followers.add(my_follower)
+            inbound_nodes.setdefault(my_follower, set()).add(id)
+            outbound_nodes.setdefault(my_follower, set())
+        
+        incomers = inbound_nodes.setdefault(id, set())
+        for my_preceeder in getattr(node, 'is_preceeded_by', []):
+            incomers.add(my_preceeder)
+            outbound_nodes.setdefault(my_preceeder, set()).add(id)
+            inbound_nodes.setdefault(my_preceeder, set())
+        
+    
+    return id_to_nodes, inbound_nodes, outbound_nodes
+
+
+def to_dot(list_of_nodes):
+    id_to_nodes, inbound_nodes, outbound_nodes = _create_adjacency_matrix(list_of_nodes)
+    
+    all_strings = ["digraph G {"]
+    
+    
+    # render all the nodes first.
+    strings = ["%s" % (id) for id in id_to_nodes.keys()]
+    
+    all_strings.append("\tnode [shape=box]; %s" % (" ".join(strings)))
+    
+    rendered = set()
+    
+    for node, inbound in inbound_nodes.items():
+        for link in inbound:
+            if (not link in rendered) and (not id_to_nodes.has_key(link)):
+                rendered.add(link)
+                all_strings.append("\tnode [shape=ellipse]; %s" % (link))
+                
+            all_strings.append("\t%s -> %s [weight = 10 color = gray];" % (link, node))
+            
+    
+    
+    
+    schedule = _create_new_node_list(id_to_nodes, inbound_nodes, outbound_nodes)
+    
+    prev = None
+    for node in schedule:
+        if prev:
+            all_strings.append("\t%s -> %s [penwidth = 5 weight = 100];" % (prev.id, node.id))
+        prev = node
+    
+    
+    all_strings.append("}")
+    return "\n".join(all_strings)
+    
+    
