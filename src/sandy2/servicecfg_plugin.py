91443ddbac2f0b1211fd9109e7dc1e4cbe3b3fdb
jameshugman <jameshugman@60b3d766-bcb7-11dd-bed2-db724ec49875>
2009-06-24 00:03:45 +0000

    Initial commit of an account registration listener: listens to an email account, waiting for registrations. Uses existing mailer and re_dispatch modules. Jimmied in into servicecfg_plugin. Not sure if this is the correct place to put it.

--- src/sandy2/servicecfg_plugin.py
+++ src/sandy2/servicecfg_plugin.py
@@ -1,27 +1,46 @@
 from sandy2.common.plugins import IPlugin
 
+from sandy2.transports.registration import AccountRegistrationListener
+
 class ServiceConfigPlugin(IPlugin):
-   def __init__(self, parser=None, db=None, properties={}):
-      self.is_preceeded_by = ['database']
-      self.is_followed_by = ['servicecfg_database']
-      self.parser = parser
-      self.database = db
-      self.properties = properties
+    def __init__(self, parser=None, db=None, properties={}):
+        self.is_preceeded_by = ['database', 'passwords']
+        self.is_followed_by = ['servicecfg_database']
+        self.parser = parser
+        self.database = db
+        self.properties = properties
+        self._account_registration = None
+      
 
-   def install(self, ctx):
-      schema = self.database.schema
-      servicecfg = schema.servicecfg('s')
+    def install(self, ctx):
+        schema = self.database.schema
+        servicecfg = schema.servicecfg('s')
 
-      table = schema.create_table(servicecfg)
-      table.service_name(str)
-      table.property_key(str)
-      table.property_val(str)
+        table = schema.create_table(servicecfg)
+        table.service_name(str)
+        table.property_key(str)
+        table.property_val(str)
+    
+        if (servicecfg._name not in self.database.get_tablenames()):
+            tx = self.database.new_transaction()
+            tx.execute(table)
+            tx.commit()
+            tx.close()
+        else:
+            print "table %s already exists.  not re-creating." % (servicecfg._name)
 
-      if (servicecfg._name not in self.database.get_tablenames()):
-         tx = self.database.new_transaction()
-         tx.execute(table)
-         tx.commit()
-         tx.close()
-      else:
-         print "table %s already exists.  not re-creating." % (servicecfg._name)
-
+    def start_up(self, ctx):
+        ar = AccountRegistrationListener()
+        
+        ctx.di.configure(ar)
+        ctx.er.account_registration.track(ar.add_rule)
+        
+        self._account_registration = ar
+    
+    def run(self):
+        print "Running polling on account registration email account %s" % (self._account_registration.registration_username)
+        self._account_registration.run()
+    
+    def stop(self):
+        if self._account_registration:
+            self._account_registration.close()
