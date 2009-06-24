a41a82aee3561501877d7a1dabd1a34794e01d5b
jameshugman <jameshugman@60b3d766-bcb7-11dd-bed2-db724ec49875>
2009-06-24 00:03:45 +0000

    Initial commit of an account registration listener: listens to an email account, waiting for registrations. Uses existing mailer and re_dispatch modules. Jimmied in into servicecfg_plugin. Not sure if this is the correct place to put it.

---
+++ trunk/src/sandy2/transports/registration.py
@@ -0,0 +1,42 @@
+from sandy2.common.messages import Message
+from sandy2.common.re_dispatch import REDispatcher
+from sandy2.transports.mailer import MailListener, MailParser
+
+SUBJECT="email_subject"
+ADDRESS="email_from"
+ALL_TEXT="email_body"
+
+class AccountRegistrationListener:
+    
+    def __init__(self, username=None, host=None, password=None):
+        
+        self.registration_username=username
+        self.registration_host=host
+        self.registration_password=password
+        
+    
+        self.dispatcher_map={}
+    
+        self.__mail_listener = None
+        self.__mail_parser = None
+    
+    def run(self):
+        self.__mail_listener = MailListener(mailhost=self.registration_host, username=self.registration_username, password=self.registration_password, listener=self.__dispatch)
+        self.__mail_parser = MailParser(self.registration_username)
+        self.__mail_listener.start()
+        
+    def close(self):
+        if self.__mail_listener:
+            self.__mail_listener.close()
+            
+    def __dispatch(self, txt):
+        email = self.__mail_parser.parse_raw_mail(txt)
+        message = Message(self.__mail_parser.find_metadata(email))
+        
+        for key, dispatcher in self.dispatcher_map.items():
+            string = message.get(key, "")
+            dispatcher.search(string, message)
+    
+    def add_rule(self, pattern, callable, match_on=ALL_TEXT):
+        dispatcher = self.dispatcher_map.setdefault(match_on, REDispatcher(None))
+        dispatcher.register_pattern(pattern, callable)
